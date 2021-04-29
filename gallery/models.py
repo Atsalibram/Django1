9609473ca67563fee210243a45d75eea3eef8d87
atsalibram <atsalibaram@gmail.com>
2021-04-29 12:23:05 +0300

    create models

--- gallery/models.py
+++ gallery/models.py
@@ -1,3 +1,71 @@
 from django.db import models
+import cloudinary
+from cloudinary.models import cloudinaryField
+import os
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=30)
+
+    def__str__(self):
+        return self.name
+
+    def save_cateory(self):
+        self.save()
+
+    def delete_category(self):
+        self.delete()
+
+class Location(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
+    def save_location(self):
+        self.save()    
+
+    def delete_location(self):
+        self.delete()
+        
+    @classmethod
+    def get_location(cls):
+        locations = Location.objects.all()
+        return locations
+
+class Image(models.Model):
+    image = CloudinaryField('image')
+    name = models.CharField(max_length=30)
+    description = models.CharField(max_length=100)
+    category = models.ForeignKey(Category,on_delete=models.CASCADE)
+    location = models.ForeignKey(Location,on_delete=models.CASCADE)
+
+     def __str__(self):
+        return self.name
+
+    def save_image(self):
+        self.save()
+
+    def delete_image(self):
+        self.delete()
+
+    @classmethod
+    def update_image(cls,id):
+       cls.objects.filter(id=id).update(image)
+
+    @classmethod
+    def get_image_by_id(cls,id):
+        image = cls.objects.filter(id=id).all()
+        return image
+
+    @classmethod
+    def search_image(cls,category):
+        images = cls.objects.filter(category__name__icontains=category)
+        return images
+
+    @classmethod
+    def filter_by_location(cls, location):
+        image_location = Image.objects.filter(location__name=location).all()
+        return image_location 
+        
+           
