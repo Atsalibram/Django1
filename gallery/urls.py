22fa92f1f9c2dc5f837fce4aaba9ef1b022824e4
atsalibram <atsalibaram@gmail.com>
2021-05-06 13:12:41 +0300

    schanged url paths

--- gallery/urls.py
+++ gallery/urls.py
@@ -1,3 +1,10 @@
+# from django.urls import path
+# from . import views
+
+# urlpatterns = [
+#     path('', views.home, name='gallery-home'),
+#     path('', views.about, name='gallery-about'),
+# ]
 from django.conf.urls import url
 from . import views
 from django.conf import settings
@@ -5,7 +12,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     url(r"^$", views.index, name="index"),
-    #url(r"^search/", views.search, name="search_results"),
+    url(r"^search/", views.search, name="search_results"),
     url(r"^browse/$", views.browse, name="browse"),
     url(r"^location/(\d+)", views.location_filter, name="location"),
 ]
